1544b935aa396c917c6814ba77405af74cb399a8
CaroBima <82395515+CaroBima@users.noreply.github.com>
2021-10-24 16:04:58 -0300

    agregada la funcionalidad en js, falta validar que se ingresen solo valores enteros

---
+++ 4 - Tp Integrador/js/scripts.js
@@ -0,0 +1,44 @@
+function calcularTotal() {
+  var cantEntradas,
+    total,
+    categoria,
+    entradaEstudiante,
+    entradaTrainee,
+    entradaJr,
+    general;
+
+  console.log('entra a la funcion');
+  cantEntradas = document.getElementById('inputCantidad').value;
+  categoria = document.getElementById('inputCategoria').value;
+
+  console.log('cant entradas ' + cantEntradas + 'categoria ' + categoria);
+
+  entradaEstudiante = 200 - (200 * 80) / 100;
+  entradaTrainee = 200 - (200 * 50) / 100;
+  entradaJr = 200 - (200 * 15) / 100;
+  general = 200;
+
+  /*verifico si el valor ingresado es mayor a cero y si es un número*/
+  if (cantEntradas > 0 && !isNaN(cantEntradas)) {
+    //verifico el tipo de entrada que es (estudiante, trainee y jr para poder mostrar el monto)
+    if (categoria == 'Estudiante') {
+      total = entradaEstudiante * cantEntradas;
+      document.getElementById('importeTotal').innerHTML =
+        'Total a Pagar: \u0024' + total;
+    } else if (categoria == 'Trainee') {
+      total = entradaTrainee * cantEntradas;
+      document.getElementById('importeTotal').innerHTML =
+        'Total a Pagar: \u0024' + total;
+    } else if (categoria == 'Junior') {
+      total = entradaJr * cantEntradas;
+      document.getElementById('importeTotal').innerHTML =
+        'Total a Pagar: \u0024' + total;
+    } else if (categoria == 'General') {
+      total = general * cantEntradas;
+      document.getElementById('importeTotal').innerHTML =
+        'Total a Pagar: \u0024' + total;
+    }
+  } else {
+    window.alert('Debe ingresar la cantidad de entradas en números');
+  }
+}
